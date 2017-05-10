d7edd7c71afd816f64fe92c38763bf189c893949
HChong <hchong7557@gmail.com>
2017-05-10 17:43:05 +0800

    Initial Commit

---
+++ MultipleChannel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipleChannel
+//
+//  Created by HChong on 2017/5/10.
+//  Copyright © 2017年 HChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
